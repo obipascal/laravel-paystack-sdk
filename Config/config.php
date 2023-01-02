d416f5b4cb320d94e4a0f56bf5a579522d4d9094
Obi Pascal <pascalobi83@gmail.com>
2023-01-02 07:44:39 +0100

    updated

--- Config/config.php
+++ Config/config.php
@@ -4,7 +4,7 @@ return [
 	/**
 	 * Paystack test or live secret key
 	 */
-	"secret" => env("PAYSTACK_SECRET", ""),
+	"secret" => env("PAYSTACK_SECRET_KEY", ""),
 	/**
 	 * Paystack API base url: This property is provided if they is any changes in
 	 * the feature.
